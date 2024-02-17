ddd688123b863624884f4e6b304548e490ff7356
José Yuri <jozeyuri@gmail.com>
2024-02-16 22:57:38 -0300

    ci(pulumi): add ecs

--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -2,8 +2,47 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const config = new pulumi.Config();
+const containerPort = config.getNumber("containerPort") || 80;
+const cpu = config.getNumber("cpu") || 512;
+const memory = config.getNumber("memory") || 128;
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// An ECS cluster to deploy into
+const cluster = new aws.ecs.Cluster("cluster", {});
+
+// An ALB to serve the container endpoint to the internet
+const loadbalancer = new awsx.lb.ApplicationLoadBalancer("loadbalancer", {});
+
+// An ECR repository to store our application's container image
+const repo = new awsx.ecr.Repository("repo", {
+    forceDelete: true,
+});
+
+// Build and publish our application's container image from ./app to the ECR repository
+const image = new awsx.ecr.Image("image", {
+    repositoryUrl: repo.url,
+    context: "./app",
+    platform: "linux/amd64",
+});
+
+// Deploy an ECS Service on Fargate to host the application container
+const service = new awsx.ecs.FargateService("service", {
+    cluster: cluster.arn,
+    assignPublicIp: true,
+    taskDefinitionArgs: {
+        container: {
+            name: "app",
+            image: image.imageUri,
+            cpu: cpu,
+            memory: memory,
+            essential: true,
+            portMappings: [{
+                containerPort: containerPort,
+                targetGroup: loadbalancer.defaultTargetGroup,
+            }],
+        },
+    },
+});
+
+// The URL at which the container's HTTP endpoint will be available
+export const url = pulumi.interpolate`http://${loadbalancer.loadBalancer.dnsName}`;

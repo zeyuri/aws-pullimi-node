846a2ed63b27ab360bdfb7017ed07267c090780f
José Yuri <jozeyuri@gmail.com>
2024-02-19 01:03:19 -0300

    feat(infra): add dns

--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -2,71 +2,42 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-const config = new pulumi.Config();
-const containerPort = config.getNumber("containerPort") || 80;
-const cpu = config.getNumber("cpu") || 512;
-const memory = config.getNumber("memory") || 128;
-// Create a security group for the load balancer
-
-const loadBalancerSecurityGroup = new aws.ec2.SecurityGroup("loadBalancerSecurityGroup", {
-    ingress: [
-        {
-            protocol: "tcp",
-            fromPort: 80,
-            toPort: 80,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-        {
-            protocol: "tcp",
-            fromPort: 443,
-            toPort: 443,
-            cidrBlocks: ["0.0.0.0/0"],
-        },
-    ],
+const pwrDnsZone = new aws.route53.Zone("pwrDnsZone", {
+  name: "thepwr.store",
 });
 
-// // Associate the security group with the load balancer
+const appCertificate = new aws.acm.Certificate("appCertificate", {
+  domainName: "app.thepwr.store",
+  validationMethod: "DNS",
+});
 
 
-// // An ECS cluster to deploy into
-// const cluster = new aws.ecs.Cluster("cluster", {});
+const appCertificateValidationRecord = new aws.route53.Record("appCertificateValidationRecord", {
+  name: appCertificate.domainValidationOptions[0].resourceRecordName,
+  type: appCertificate.domainValidationOptions[0].resourceRecordType,
+  zoneId: pwrDnsZone.zoneId,
+  records: [appCertificate.domainValidationOptions[0].resourceRecordValue],
+  ttl: 300,
+});
 
-// // An ALB to serve the container endpoint to the internet
-// const loadbalancer = new awsx.lb.ApplicationLoadBalancer("loadbalancer", {
-//   securityGroups: [loadBalancerSecurityGroup.id],
-// });
+const certificateValidation = new aws.acm.CertificateValidation("certificateValidation", {
+  certificateArn: appCertificate.arn,
+  validationRecordFqdns: [appCertificateValidationRecord.fqdn],
+});
 
-
-// // An ECR repository to store our application's container image
-// const repo = new awsx.ecr.Repository("repo", {
-//     forceDelete: true,
-// });
-
-// // Build and publish our application's container image from ./app to the ECR repository
-// const image = new awsx.ecr.Image("image", {
-//     repositoryUrl: repo.url,
-//     context: "./app",
-//     platform: "linux/amd64",
-// });
-
-// // Deploy an ECS Service on Fargate to host the application container
-// const service = new awsx.ecs.FargateService("service", {
-//     cluster: cluster.arn,
-//     assignPublicIp: true,
-//     taskDefinitionArgs: {
-//         container: {
-//             name: "app",
-//             image: image.imageUri,
-//             cpu: cpu,
-//             memory: memory,
-//             essential: true,
-//             portMappings: [{
-//                 containerPort: containerPort,
-//                 targetGroup: loadbalancer.defaultTargetGroup,
-//             }],
-//         },
-//     },
-// });
-
-// // The URL at which the container's HTTP endpoint will be available
-// export const url = pulumi.interpolate`http://${loadbalancer.loadBalancer.dnsName}`;
+const loadBalancerSecurityGroup = new aws.ec2.SecurityGroup("loadBalancerSecurityGroup", {
+  ingress: [
+      {
+          protocol: "tcp",
+          fromPort: 80,
+          toPort: 80,
+          cidrBlocks: ["0.0.0.0/0"],
+      },
+      {
+          protocol: "tcp",
+          fromPort: 443,
+          toPort: 443,
+          cidrBlocks: ["0.0.0.0/0"],
+      },
+  ],
+});

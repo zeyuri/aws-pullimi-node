70ee691cce283de1063c1df7a27b1fbc8f774294
José Yuri <jozeyuri@gmail.com>
2024-02-22 16:56:37 -0300

    feat(nitro): add app example

---
+++ apps/nitro/routes/health.get.ts
@@ -0,0 +1,3 @@
+export default eventHandler(async () => {
+  return { message: "OK" };
+});
--- packages/infra/index.ts
+++ packages/infra/index.ts
@@ -30,11 +30,124 @@ const certificateValidation = new aws.acm.CertificateValidation(
   },
 );
 
-// const vpc = new awsx.ec2.Vpc("main-vpc", {
-//   cidrBlock: "10.0.0.0/16",
-//   subnetSpecs: [
+// const vpc = new awsx.ec2.Vpc("vpc", {});
+
+// const targetGroup = new aws.lb.TargetGroup("targetGroup", {
+//   port: 80,
+//   ipAddressType: "ipv4",
+//   healthCheck: {
+//     path: "/health",
+//     protocol: "HTTP",
+//   },
+//   protocol: "HTTP",
+//   targetType: "ip",
+//   vpcId: vpc.vpcId,
+// });
+
+// const loadBalancerSecurityGroup = new aws.ec2.SecurityGroup(
+//   "loadBalancerSecurityGroup",
+//   {
+//     ingress: [
+//       {
+//         protocol: "tcp",
+//         fromPort: 80,
+//         toPort: 80,
+//         cidrBlocks: ["0.0.0.0/0"],
+//       },
+//       {
+//         protocol: "tcp",
+//         fromPort: 443,
+//         toPort: 443,
+//         cidrBlocks: ["0.0.0.0/0"],
+//       },
+//     ],
+//   },
+// );
+
+// const loadbalancer = new awsx.lb.ApplicationLoadBalancer("loadbalancer", {
+//   securityGroups: [loadBalancerSecurityGroup.id],
+//   listeners: [
 //     {
-//       type: "Public",
+//       alpnPolicy: "HTTP2Preferred",
+//       certificateArn: appCertificate.arn,
+//       port: 443,
+//       protocol: "HTTPS",
+//       sslPolicy: "ELBSecurityPolicy-TLS13-1-2-2021-06",
+//       defaultActions: [
+//         {
+//           type: "forward",
+//           targetGroupArn: targetGroupt.arn,
+//         },
+//       ]
+//     },
+//     {
+//       port: 80,
+//       protocol: "HTTP",
+//       defaultActions: [
+//         {
+//           type: "forward",
+//           targetGroupArn: targetGroupt.arn,
+//         },
+//       ],
 //     },
 //   ],
 // });
+
+const repo = new awsx.ecr.Repository("repo", {
+  forceDelete: true,
+});
+
+// Build and publish our application's container image from ./app to the ECR repository.
+const image = new awsx.ecr.Image("image", {
+  repositoryUrl: repo.url,
+  dockerfile: "./Dockerfile",
+  context: "../../",
+  args: {
+    NODE_ENV: "production",
+    PROJECT: "nitro",
+  },
+  builderVersion: "BuilderBuildKit",
+});
+
+const cluster = new awsx.classic.ecs.Cluster("cluster");
+
+const alb = new awsx.classic.lb.ApplicationLoadBalancer("net-lb", {
+  external: true,
+  securityGroups: cluster.securityGroups,
+});
+
+const targetGroup = alb.createTargetGroup("targetGroup", {
+  port: 80,
+  protocol: "HTTP",
+  healthCheck: {
+    path: "/health",
+    port: "3000",
+    protocol: "HTTP",
+  },
+});
+const httpListener = alb.createListener("http", {
+  port: 80,
+  external: true,
+  targetGroup: targetGroup,
+});
+const httpsListener = alb.createListener("https", {
+  port: 443,
+  external: true,
+  certificateArn: appCertificate.arn,
+  protocol: "HTTPS",
+  sslPolicy: "ELBSecurityPolicy-TLS13-1-2-2021-06",
+  targetGroup: targetGroup,
+});
+
+const appService = new awsx.classic.ecs.FargateService("app-svc", {
+  cluster,
+  taskDefinitionArgs: {
+    container: {
+      portMappings: [targetGroup],
+      image: image.imageUri,
+      cpu: 128 /*10% of 1024*/,
+      memory: 256 /*MB*/,
+    },
+  },
+  desiredCount: 1,
+});
